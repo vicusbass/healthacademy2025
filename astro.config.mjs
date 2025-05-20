00cd03d59b693786a23a8b75ca27a59eeeb32e7d
Vasile Pop <vasile.pop@gmail.com>
2025-05-20 21:52:22 +0300

    initial push

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import alpinejs from '@astrojs/alpinejs';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [alpinejs(), icon()]
+});
