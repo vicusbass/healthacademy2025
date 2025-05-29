78075db9156e461f045bb78d5b3852e343b90851
Vasile Pop <vasile.pop@gmail.com>
2025-05-29 11:17:18 +0300

    remove html folder

--- src/assets/tech/motion-guidance.jpg_files/custom.js
+++
@@ -1,2 +0,0 @@
-$(document).ready(function(){jQuery('a[href^="http"]').not('a[href^="'+$(location).attr("hostname")+'"]').attr("target","_blank")});
-//# sourceMappingURL=/cdn/shop/t/10/assets/custom.js.map?v=16588435640219163061627076140
--- src/assets/tech/motion-guidance.jpg_files/motion-guidance.myshopify.com.js
+++
@@ -1,1 +0,0 @@
-var bundler_settings_updated='1712113337';
